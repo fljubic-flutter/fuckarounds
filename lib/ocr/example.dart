682f11aaf6b25f177dfb78f10eab7aeafce64837
Filip Ljubic <f.ljubic7@hotmail.com>
2020-09-23 23:51:32 +0200

    testing out ocr

---
+++ lib/ocr/example.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:translator/translator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Realtime translation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Realtime translation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  File _pickedImage;
+  bool _isImageLoaded = false;
+  bool _isTextLoaded = false;
+  String _text = "";
+
+  Future pickImage() async {
+    var tempStore = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _pickedImage = tempStore;
+      _isImageLoaded = true;
+    });
+  }
+
+  Future readText() async {
+    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(_pickedImage);
+    TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizeText.processImage(ourImage);
+
+    translate(readText.text);
+  }
+
+  void translate(String readText) async {
+    GoogleTranslator translator = GoogleTranslator();
+
+    String input = readText;
+
+    translator
+        .translate(input, to: 'pt')
+        .then((_translatedText) => setState(() {
+              _text = _translatedText.text;
+              _isTextLoaded = true;
+            }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            _isImageLoaded
+                ? Center(
+                    child: Container(
+                      height: 200.0,
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: FileImage(_pickedImage),
+                              fit: BoxFit.cover)),
+                    ),
+                  )
+                : Container(),
+            _isTextLoaded ? Center(child: Text(_text)) : Container(),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              child: Text('Choose an image'),
+              onPressed: pickImage,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              child: Text('Read Text'),
+              onPressed: readText,
+            ),
+          ],
+        ));
+  }
+}
